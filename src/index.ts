401cafd03600c408b2cf4c6798c2a2327706ba46
liukun <919590347@qq.com>
2019-11-26 22:24:07 +0800

    文件整理

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 
-// import './type.ts'
-// import './enum.ts'
-// import './types/fn.ts'
-// import './types/10.class.ts'
-// import './types/11.abstract.ts'
-// import './types/13.T.ts'
-// import './type-checking-mechanism/15.1.type-inference.ts'
-// import './type-checking-mechanism/18. advanced-cross-union'
-import './type-checking-mechanism/19. index-type'
+// import './chapter1/type.ts'
+// import './chapter1/enum.ts'
+// import './chapter1/types/fn.ts'
+// import './chapter1/types/10.class.ts'
+// import './chapter1/types/11.abstract.ts'
+// import './chapter1/types/13.T.ts'
+// import './chapter1/type-checking-mechanism/15.1.type-inference.ts'
+// import './chapter1/type-checking-mechanism/18. advanced-cross-union.ts'
+// import './chapter1/type-checking-mechanism/19. index-type.ts'
 
 let hello: string = 'hello tsc'
 document.querySelectorAll('.app')[0].innerHTML = hello
