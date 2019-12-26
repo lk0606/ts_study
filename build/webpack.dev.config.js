66733c40564e356a85272085576c8ecdd0b7eaa3
liukun <kun.liu@winbaoxian.com>
2019-12-26 12:56:05 +0800

    react工程化开始

---
+++ build/webpack.dev.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    /**
+     * cheap 忽略类信息
+     * module 定位ts源码
+     */
+    // devtool: 'cheap-module-eval-source-map',
+    devtool: 'module-eval-source-map',
+    devServer: {
+        port: 2000
+    }
+}
